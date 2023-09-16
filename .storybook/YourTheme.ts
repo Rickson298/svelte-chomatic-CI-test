dbaa11042848dce1e109e599e5473d295bdb1eee
Rickson Oliveira <rickson.patrick13@gmail.com>
2023-09-16 13:34:37 -0300

    feat: configure Theming

---
+++ .storybook/YourTheme.ts
@@ -0,0 +1,9 @@
+import { themes } from '@storybook/theming';
+import { create } from '@storybook/theming/create';
+
+export default create({
+	base: 'dark',
+	fontBase: '"Open Sans", sans-serif',
+	inputBorderRadius: 6,
+	brandTitle: 'ApartApe UI'
+});
